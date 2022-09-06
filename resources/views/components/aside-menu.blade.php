a725ac69bc334c7e2edf4279358c72f02c9fb957
Ahmed Hasan <hlhatab@gmail.com>
2022-09-06 12:03:30 +0300

    Fixed

--- resources/views/components/aside-menu.blade.php
+++ resources/views/components/aside-menu.blade.php
@@ -69,7 +69,7 @@
                         </a>
                     </div>
                 </div>
-                
+
                 <div class="menu-item">
                     <div class="menu-content pt-8 pb-2">
                         <span class="menu-section text-muted text-uppercase fs-8 ls-1" style="margin-left: -12px">
--- resources/views/pages/main_page/product/index.blade.php
+++ resources/views/pages/main_page/product/index.blade.php
@@ -3,7 +3,7 @@
 @section('page_title', 'Products')
 
 @section('css')
-    {{--    <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"/>--}}
+    {{-- <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"/> --}}
 @endsection
 
 @section('nav_title', 'Products')
@@ -20,7 +20,8 @@
                         </h4>
                         <div class="col-m-12">
                             <i class="far fa-lightbulb fs-2 mt-3" style="position: absolute; left: 15px;"></i>
-                            <input type="text" data-kt-filter="search" class="form-control form-control-solid w-250px ps-14" placeholder="Search Report" />
+                            <input type="text" data-kt-filter="search"
+                                class="form-control form-control-solid w-250px ps-14" placeholder="Search Report" />
                         </div>
                     </div>
 
@@ -37,24 +38,24 @@
         <div class="card-body">
             <table class="table align-middle border rounded table-row-dashed fs-6 g-5" id="kt_datatable_example_1">
                 <thead>
-                <!--begin::Table row-->
-                <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase">
-                    <th class="min-w-100px">Product Name</th>
-                    <th class="min-w-100px">Category</th>
-                    <th class="min-w-100px">Price</th>
-                    <th class="min-w-100px">Cover Image</th>
-                    <th class="text-center min-w-100px pe-5">Operations</th>
-                </tr>
-                <!--end::Table row-->
+                    <!--begin::Table row-->
+                    <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase">
+                        <th class="min-w-100px">Product Name</th>
+                        <th class="min-w-100px">Category</th>
+                        <th class="min-w-100px">Price</th>
+                        <th class="min-w-100px">Cover Image</th>
+                        <th class="text-center min-w-100px pe-5">Operations</th>
+                    </tr>
+                    <!--end::Table row-->
                 </thead>
                 <tbody class="fw-bold text-gray-600">
-                    @foreach( $products as $product )
+                    @foreach ($products as $product)
                         <tr class="odd">
                             <td>
                                 {{ $product->product_name }}
                             </td>
                             <td>
-                                @if($product->category)
+                                @if ($product->category)
                                     {{ $product->category->category_name }}
                                 @else
                                     {{ 'No Category' }}
@@ -64,17 +65,20 @@
                                 {{ $product->price }}
                             </td>
                             <td class="text-end pe-0">
-                                <img src="{{ asset('storage') . '/' . $product->cover_image }}" width="50" height="50">
+                                <img src="{{ asset('storage') . '/' . $product->cover_image }}" width="50"
+                                    height="50">
                             </td>
                             <td class="text-end d-flex justify-content-end ">
-                                <a href="{{ route('product.edit', ['id' => $product->id]) }}" class="btn btn-primary" style="width: 90px; margin-right: 5px">
+                                <a href="{{ route('product.edit', ['id' => $product->id]) }}" class="btn btn-primary"
+                                    style="width: 90px; margin-right: 5px">
                                     <i class="fa-solid fa-pen-to-square" style="margin-top: -2px"></i>
                                     <span>
-                                    Edit
-                                </span>
+                                        Edit
+                                    </span>
                                 </a>
 
-                                <form action="{{ route('product.delete', ['id' => $product->id]) }}" method="POST" style="margin-right: 5px">
+                                <form action="{{ route('product.delete', ['id' => $product->id]) }}" method="POST"
+                                    style="margin-right: 5px">
                                     @csrf
                                     @method('DELETE')
 
@@ -86,7 +90,8 @@
                                     </button>
                                 </form>
 
-                                <a href="{{ route('product.productShow', ['id' => $product->id]) }}" class="btn btn-warning" style="margin-right: 5px">
+                                <a href="{{ route('product.productShow', ['id' => $product->id]) }}" class="btn btn-warning"
+                                    style="margin-right: 5px">
                                     <i class="fa-solid fa-eye" style="margin-top: -2px"></i>
                                     <span>
                                         Show
@@ -109,19 +114,20 @@
         "use strict";
 
         // Class definition
-        var KTDatatablesButtons = function () {
+        var KTDatatablesButtons = function() {
             // Shared variables
             var table;
             var datatable;
 
             // Private functions
-            var initDatatable = function () {
+            var initDatatable = function() {
                 // Set date data order
                 const tableRows = table.querySelectorAll('tbody tr');
 
                 tableRows.forEach(row => {
                     const dateRow = row.querySelectorAll('td');
-                    const realDate = moment(dateRow[3].innerHTML, "DD MMM YYYY, LT").format(); // select date from 4th column in table
+                    const realDate = moment(dateRow[3].innerHTML, "DD MMM YYYY, LT")
+                .format(); // select date from 4th column in table
                     dateRow[3].setAttribute('data-order', realDate);
                 });
 
@@ -137,8 +143,7 @@
             var exportButtons = () => {
                 const documentTitle = 'Customer Orders Report';
                 var buttons = new $.fn.dataTable.Buttons(table, {
-                    buttons: [
-                        {
+                    buttons: [{
                             extend: 'copyHtml5',
                             title: documentTitle
                         },
@@ -158,14 +163,16 @@
                 }).container().appendTo($('#kt_datatable_example_1_export'));
 
                 // Hook dropdown menu click event to datatable export buttons
-                const exportButtons = document.querySelectorAll('#kt_datatable_example_1_export_menu [data-kt-export]');
+                const exportButtons = document.querySelectorAll(
+                    '#kt_datatable_example_1_export_menu [data-kt-export]');
                 exportButtons.forEach(exportButton => {
                     exportButton.addEventListener('click', e => {
                         e.preventDefault();
 
                         // Get clicked export value
                         const exportValue = e.target.getAttribute('data-kt-export');
-                        const target = document.querySelector('.dt-buttons .buttons-' + exportValue);
+                        const target = document.querySelector('.dt-buttons .buttons-' +
+                        exportValue);
 
                         // Trigger click event on hidden datatable export buttons
                         target.click();
@@ -176,17 +183,17 @@
             // Search Datatable --- official docs reference: https://datatables.net/reference/api/search()
             var handleSearchDatatable = () => {
                 const filterSearch = document.querySelector('[data-kt-filter="search"]');
-                filterSearch.addEventListener('keyup', function (e) {
+                filterSearch.addEventListener('keyup', function(e) {
                     datatable.search(e.target.value).draw();
                 });
             }
 
             // Public methods
             return {
-                init: function () {
+                init: function() {
                     table = document.querySelector('#kt_datatable_example_1');
 
-                    if ( !table ) {
+                    if (!table) {
                         return;
                     }
 
@@ -198,53 +205,9 @@
         }();
 
         // On document ready
-        KTUtil.onDOMContentLoaded(function () {
+        KTUtil.onDOMContentLoaded(function() {
             KTDatatablesButtons.init();
         });
-    </script>
-
-    <script>
-        $(document).ready(function() {
-            $('#MPheader_button_action').change(function() {
-                if ($(this).val() == 1) {
-
-                    $.ajax({
-                        type: 'put',
-                        url: 'box_header/updateButtonActive',
-                        headers: {'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')},
-                        data:{
-                            action: 0,
-                        },
-                        dataType: 'json',
-                        success: function(data) {
-                            console.log(data.action)
-                        },
-                        error: function(data) {
-                            console.log(data)
-                        },
-                    })
-
-                }else if ($(this).val() == 0){
-
-                    $.ajax({
-                        type: 'put',
-                        url: 'box_header/updateButtonActive',
-                        headers: {'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')},
-                        data:{
-                            action: 1,
-                        },
-                        dataType: 'json',
-                        success: function(data) {
-                            console.log(data.action)
-                        },
-                        error: function(data) {
-                            console.log(data)
-                        },
-                    })
-                };
-            })
-        })
-
     </script>
 
 @endsection
